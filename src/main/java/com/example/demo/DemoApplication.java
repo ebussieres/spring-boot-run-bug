c67ad3ba5a9412b308c890b48597d15ddb4347f9
Eric Bussieres <eric.bussieres@ssq.ca>
2020-01-31 13:00:33 -0500

    Demo for bug with spring-boot run

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    Arrays.stream(args).forEach(System.out::println);
+  }
+}
